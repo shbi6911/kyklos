9bafb9c4932ea6a8153fa417c88879bd776fddec
Shane Billingsley <shbi6911@colorado.edu>
2026-01-07 20:37:43 -0700

    fixed ReadtheDocs config

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -7,6 +7,8 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../../src'))
+# Mock imports for packages not available on RTD
+autodoc_mock_imports = ['heyoka']
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
